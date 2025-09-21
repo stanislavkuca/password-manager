35c3e867b305630c04cb36518741dacb32273773
stanislavkuca <kucastanik@gmail.com>
2025-09-21 21:58:33 +0200

    feat: make "New account" button functional
    - add "New account" dialog window with input form
    - connect "New account" button to open the dialog

---
+++ PasswordManager.UI/Views/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PasswordManager.UI.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void NewAccountButton_Click(object sender, RoutedEventArgs e)
+        {
+            var newAccountWindow = new NewAccountWindow
+            {
+                Owner = this,
+                WindowStartupLocation = WindowStartupLocation.CenterOwner
+            };
+
+            if (newAccountWindow.ShowDialog() == true)
+            {
+                var account = newAccountWindow.CreatedAccount;
+                AccountList.Items.Add(account);
+            }
+        }
+    }
+}
---
+++ PasswordManager.UI/Views/NewAccountWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PasswordManager.UI.Views
+{
+    /// <summary>
+    /// Interaction logic for NewAccountWindow.xaml
+    /// </summary>
+    public partial class NewAccountWindow : Window
+    {
+        public class Account(string name, string username, string password, string? note = null)
+        {
+            public string AccountName { get; } = name;
+            public string AccountUsername { get; } = username;
+            public string AccountPassword { get; } = password;
+            public string? AccountNote { get; } = note;
+        }
+        
+        public Account? CreatedAccount { get; private set; }
+
+        public NewAccountWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            CreatedAccount = new Account(
+                AccountNameTextBox.Text,
+                AccountUsernameTextBox.Text,
+                AccountPasswordTextBox.Text,
+                AccountNoteTextBox.Text
+            );
+
+            DialogResult = true;
+            Close();
+        }
+
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+            Close();
+        }
+    }
+}
