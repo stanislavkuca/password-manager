abd59e96519c717a1801263b802f02b90eb61a62
Stanik Kuča <kucastanik@gmail.com>
2026-03-15 13:57:40 +0100

    feat: working on master password

---
+++ PasswordManager/Services/AuthService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace PasswordManager.Services
+{
+    public static class AuthService
+    {
+        public static string Hash(string input)
+        {
+            using var sha = SHA256.Create();
+            var bytes = sha.ComputeHash(Encoding.UTF8.GetBytes(input));
+            return Convert.ToBase64String(bytes);
+        }
+
+        public static bool Verify(string password, string storedHash)
+        {
+            return Hash(password) == storedHash;
+        }
+    }
+}
