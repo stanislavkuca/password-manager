2e6995ddd6e8fc70d10f1040027fc88069c643d6
Stanik Kuča <kucastanik@gmail.com>
2026-03-16 18:34:58 +0100

    feat: auth service

---
+++ PasswordManager/Views/LoginDialog.xaml.cs
@@ -0,0 +1,48 @@
+﻿using PasswordManager.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PasswordManager.Views
+{
+    /// <summary>
+    /// Interaction logic for LoginDialog.xaml
+    /// </summary>
+    public partial class LoginDialog : Window
+    {
+        public LoginDialog()
+        {
+            InitializeComponent();
+        }
+
+        private void Login_Click(object sender, RoutedEventArgs e)
+        {
+            string entered = MasterPasswordBox.Password;
+
+            if (AuthService.Verify(entered, storedHash))
+            {
+                DialogResult = true;
+            }
+            else
+            {
+                MessageBox.Show("Wrong password");
+            }
+        }
+
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+            Close();
+        }
+    }
+}
