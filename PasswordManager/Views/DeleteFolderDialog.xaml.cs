2a07a49ec31d9150f84d60ef642381781a1bb6af
Stanislav Kuca <kucastanik@gmail.com>
2026-04-04 00:49:38 +0200

    chore: rename delete dialogs

---
+++ PasswordManager/Views/DeleteFolderDialog.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows;
+using PasswordManager.Models;
+
+namespace PasswordManager.Views
+{
+    public enum DeleteFolderResult { OnlyFolder, Everything, Cancel }
+
+    public partial class DeleteFolderDialog : Window
+    {
+        public DeleteFolderResult Result { get; private set; } = DeleteFolderResult.Cancel;
+
+        public DeleteFolderDialog() => InitializeComponent();
+
+        private void DeleteFolderOnlyBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Result = DeleteFolderResult.OnlyFolder;
+            DialogResult = true;
+        }
+
+        private void DeleteEverythingBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Result = DeleteFolderResult.Everything;
+            DialogResult = true;
+        }
+    }
+}
