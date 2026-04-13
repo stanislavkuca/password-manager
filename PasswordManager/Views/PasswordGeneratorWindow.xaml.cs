9789d29e34c8b975255a051279f48d1d3fccedaa
Stanislav Kuca <kucastanik@gmail.com>
2026-04-13 21:02:08 +0200

    wip: random password generation

---
+++ PasswordManager/Views/PasswordGeneratorWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PasswordManager.Views
+{
+    /// <summary>
+    /// Interaction logic for PasswordGeneratorWindow.xaml
+    /// </summary>
+    public partial class PasswordGeneratorWindow : Window
+    {
+        public PasswordGeneratorWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void LengthSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+
+        }
+    }
+}
